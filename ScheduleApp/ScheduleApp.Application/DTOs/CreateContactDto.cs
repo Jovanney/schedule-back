fa9b8d8fce0fcaf120c52aec2062d71c115efc70
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-12-16 22:13:48 -0300

    feat: Add CreateContactDto for creating new contact entries

---
+++ ScheduleApp/ScheduleApp.Application/DTOs/CreateContactDto.cs
@@ -0,0 +1,8 @@
+namespace ScheduleApp.Application.DTOs;
+
+public class CreateContactDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+}
